0f989211b904d6e599c48dd210b14351830a6064
Mohamed Nasser <aheart@500@yahoo.com>
2020-09-21 01:28:15 +0200

    second-commit

--- dist/models/Contact.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var Contact = new mongoose_1.Schema({
-    name: String,
-    number: String,
-    owner: String,
-});
-exports.default = mongoose_1.model("Contact", Contact);
